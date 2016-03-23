0c3148e20d91b32ef0d7298209e231f1172efcbf
Kartik Venkat <kartik.johncena@gmail.com>
2016-03-23 16:25:34 -0400

    Added files via upload

--- Titanic exercise/titanic.R
+++ Titanic exercise/titanic.R
@@ -1,14 +1,29 @@
 
-titanic <- read_excel(path = "C:/Springboard Data Science work/Titanic project/titanic_original.xls")
+library(dplyr)
 
+library(readxl)
+
+library(tidyr)
+
+library(readr)
+
+#Load data in RStudio
+titanic <- read_excel(path = "C:/Springboard/titanic_original.xls")
+
+#Replace missing value
 titanic$embarked[titanic$cabin == 'B28'] <- 'S'
 
+#Calculate mean and replace NA values
 titanic$age[is.na(titanic$age)] <- 0
 
 age_mean <- mean(titanic$age)
 
-age_mean
-
 titanic$age[titanic$age == 0] <- age_mean
 
+#Fill empty slots with dummy value
 titanic$boat[is.na(titanic$boat)] <- 'NA'
+
+#Create new cabin number column
+titanic$has_cabin_number <- ifelse(is.na(titanic$cabin),0,1)
+
+write_csv(titanic, path = "C:/Springboard/titanic_clean.csv")
