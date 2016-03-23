81004568b318334265fc646648014a63783ca406
Kartik Venkat <kartik.johncena@gmail.com>
2016-03-22 20:58:55 -0400

    Update titanic.R

--- Titanic exercise/titanic.R
+++ Titanic exercise/titanic.R
@@ -1,4 +1,10 @@
 
 titanic <- read_excel(path = "C:/Springboard Data Science work/Titanic project/titanic_original.xls")
 
+titanic$embarked[titanic$cabin == 'B28'] <- 'S'
 
+age_mean <- mean(titanic$age)
+
+age_people <- filter(titanic, age != 'NA')
+
+age_people <- as.vector(age_people)
