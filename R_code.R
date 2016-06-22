6f870a2325e27f46c5188f2c4cfa412fece3f9c3
Kartik Venkat <kartik.johncena@gmail.com>
2016-06-22 10:36:17 -0400

    Update R_code.R

--- R_code.R
+++ R_code.R
@@ -14,10 +14,11 @@ library(Hmisc)
 library(mice)
 library(jsonlite)
 library(ROCR)
+library(xlsx)
 
 ## Load the full bank dataset
 
-bankFull <- read.csv("bank-additional-full.csv", sep = ';')
+bankFull <- read.csv("bank-additional-full-clean.csv")
 
 View(bankFull)
 
@@ -29,32 +30,6 @@ str(bankFull1)
 
 ## Logistic regression model with marital and job as inputs
 
-model1 <- glm(y ~ marital + job, data = bankFull, family = binomial)
+model1 <- glm(y ~ ., data = bankFull1, family = binomial)
 
-summary(model1)
-
-## Add year field to dataset
-
-## Assigning year to records - problem statement mentions year range from May 2008 to Nov 2010
-
-bankFull <- bankFull %>%
-  slice(1:27691) %>%
-  mutate(year = 2008)
-
-bankFull <- bankFull %>%
-  slice(27692:39131) %>%
-  mutate(year = 2009)
-
-bankFull <- bankFull %>%
-  slice(39132:41189) %>%
-  mutate(year = 2010)
-
-View(bankFull)
-
-
-
-
-
-
-
-
+coef(summary(model1))
