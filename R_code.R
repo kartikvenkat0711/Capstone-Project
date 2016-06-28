c84ba8e34de5085a56412ecf93fa55f00d6b7107
Kartik Venkat <kartik.johncena@gmail.com>
2016-06-28 14:18:52 -0400

    Update R_code.R

--- R_code.R
+++ R_code.R
@@ -26,10 +26,83 @@ View(bankFull)
 
 bankFull1 <- select(bankFull, -duration)
 
-str(bankFull1)
+View(bankFull1)
+
+## Load training data as new data frame
+
+bankTest <- read.csv("bank-additional.csv")
+
+str(bankTest)
 
 ## Logistic regression model with marital and job as inputs
 
 model1 <- glm(y ~ ., data = bankFull1, family = binomial)
 
-coef(summary(model1))
+summary(model1)
+
+predictBank <- predict(model1, type = "response")
+
+ROCRpred1 <- prediction(predictBank, bankFull1$y)
+
+ROCRperf1 <- performance(ROCRpred1, "tpr", "fpr")
+
+ROCRauc1 <- performance(ROCRpred1, "auc")
+
+ROCRauc1@y.values ## AUC keeping the missing values
+
+plot(ROCRperf1, colorize = TRUE)
+
+## End of prediction for data w/ missing values
+
+summary(predictBank)
+
+table(bankFull1$job, bankFull1$y)
+
+model3 <- glm(y ~ housing, data = bankFull1, family = binomial)
+
+summary(model3)
+
+summary(bankFull1$job)
+
+bankFull2 <- bankFull1 %>%
+  arrange(marital) %>%
+  slice(1:41108)
+
+table(bankFull3$job, bankFull3$y)
+
+summary(bankFull2$marital)
+
+bankFull3 <- bankFull2 %>%
+  arrange(marital) %>%
+  slice(1:40787)
+
+View(bankFull3)
+
+summary(bankFull3$marital)
+
+## Model for data w/o missing values
+
+model4 <- glm(y ~ ., data = bankFull3, family = binomial)
+
+coef(summary(model4))
+
+predictTrain <- predict(model4, type = "response")
+
+table(bankFull3$y, predictTrain > 0.5)
+
+2053/(2053+2541) ## Sensitivity
+
+35650/(35650+543) ## Specificity
+
+ROCRpred <- prediction(predictTrain, bankFull3$y)
+
+ROCRperf <- performance(ROCRpred, "tpr", "fpr")
+
+ROCRauc <- performance(ROCRpred, "auc")
+
+ROCRauc@y.values ## AUC w/o missing values
+
+plot(ROCRperf, colorize = TRUE)
+
+## End of model for data w/o missing values
+
